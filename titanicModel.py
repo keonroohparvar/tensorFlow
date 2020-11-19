81534425a331da1d446b9789877973683d3d584b
Keon <kroohpar@gmail.com>
2020-11-18 22:39:42 -0800

    First Commit

---
+++ titanicModel.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+import tensorflow as tf
+import tensorflow.compat.v2.feature_column as fc
+
+
+print("Hello world!") 
+printf("Yeet.")
