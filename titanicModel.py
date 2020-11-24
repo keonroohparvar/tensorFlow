7e4c9eed79d77c336c4222204a438513cee370f1
Keon <kroohpar@gmail.com>
2020-11-23 21:45:46 -0800

    Done with lienar Regression model

--- titanicModel.py
+++ titanicModel.py
@@ -42,8 +42,25 @@ def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32)
         if shuffle:
             ds = ds.shuffle(1000) # Randomize order of data
         ds = ds.batch(batch_size).repeat(num_epochs) # Split data into batches of 32 and repeat number of epoch times
-        return ds
-    return input_function()
+        return ds # Return a batch of the dataset
+    return input_function
 
+train_input_fn = make_input_fn(dftrain, y_train) # makes input for training data
+eval_input_fn = make_input_fn(dfeval, y_eval) # makes input for evaluation data
 
+# Creating linear model
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
 
+# Training the model
+linear_est.train(train_input_fn)
+
+# Get model metrics / stats by testing on test data
+result = linear_est.evaluate(eval_input_fn)
+
+# The following will print the result of the accuracy
+#clear_output() # Clears console output
+#print(result)
+
+# The following is how you predict in Linear Models
+result = list(linear_est.predict(eval_input_fn))
+print(result[0])
